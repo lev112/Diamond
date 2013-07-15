3c03e37c551dc9a51e5dd08b51925294e288930f
lev <lev@qlika.com>
2013-07-15 17:33:20 +0300

    fix

---
+++ src/collectors/postgres_single_table/postgres_single_table_post_collect.py
@@ -0,0 +1,7 @@
+from postgres_single_table import PostgresqlSingleDbCollector
+from diamond.post_collect_mixin import PostCollectMixin
+
+__author__ = 'Lev'
+
+class PostgresqlSingleDbPostCollector(PostCollectMixin, PostgresqlSingleDbCollector):
+    pass
