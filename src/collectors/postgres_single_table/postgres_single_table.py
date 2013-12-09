1956c2aa09d9846f8d28cea0a4bc33be943d2f11
lev <lev@qlika.com>
2013-12-09 14:22:53 +0200

    ignore rdsadmin db

--- src/collectors/postgres_single_table/postgres_single_table.py
+++ src/collectors/postgres_single_table/postgres_single_table.py
@@ -3,26 +3,55 @@ from postgres import *
 
 __author__ = 'Lev'
 
+
+class DatabaseStatsRDS(DatabaseStats):
+    """
+    Database-level summary stats
+    ignore the rdsadmin table as well
+    """
+    query = """
+        SELECT pg_stat_database.datname as datname,
+               pg_stat_database.numbackends as numbackends,
+               pg_stat_database.xact_commit as xact_commit,
+               pg_stat_database.xact_rollback as xact_rollback,
+               pg_stat_database.blks_read as blks_read,
+               pg_stat_database.blks_hit as blks_hit,
+               pg_stat_database.tup_returned as tup_returned,
+               pg_stat_database.tup_fetched as tup_fetched,
+               pg_stat_database.tup_inserted as tup_inserted,
+               pg_stat_database.tup_updated as tup_updated,
+               pg_stat_database.tup_deleted as tup_deleted,
+               pg_database_size(pg_database.datname) AS size
+        FROM pg_database
+        JOIN pg_stat_database
+        ON pg_database.datname = pg_stat_database.datname
+        WHERE pg_stat_database.datname
+        NOT IN ('template0','template1','postgres','rdsadmin')
+    """
 registry['extended'] = (
-    DatabaseStats,
+    DatabaseStatsRDS,
     DatabaseConnectionCount,
     UserTableStats,
     UserIndexStats,
     UserTableIOStats,
     UserIndexIOStats,
-    ConnectionStateStats,
+    # ConnectionStateStats,
     LockStats,
     RelationSizeStats,
     BackgroundWriterStats,
     # WalSegmentStats,
     TransactionCount,
-    IdleInTransactions,
-    LongestRunningQueries,
-    UserConnectionCount,
+    # IdleInTransactions,
+    # LongestRunningQueries,
+    # UserConnectionCount,
     TableScanStats,
     TupleAccessStats,
 )
 
+registry['basic']= (
+        DatabaseStatsRDS,
+        DatabaseConnectionCount,
+    )
 
 
 
